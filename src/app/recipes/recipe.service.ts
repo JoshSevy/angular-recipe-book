e42702e238adad38c56cb091bf5d6175fc4d38f7
JoshSevy <joshuasevy@outlook.com>
2020-12-05 18:25:12 -0700

    Add ingredients to recipe items list them on recipe details page

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 import { Recipe } from './recipe.model';
 
@@ -6,8 +7,24 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2019/06/Thai-Cucumber-Salad-Recipe-3.jpg'),
-    new Recipe('Hamburger', 'A delicious perfectly cooked hamburger', 'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty Shnitzel - just awesome!', 
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger', 
+      'A delicious perfectly cooked hamburger', 
+      'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2),
+        new Ingredient('tomato', 1),
+        new Ingredient('onion', 2)
+      ])
   ];
 
   getRecipes() {
