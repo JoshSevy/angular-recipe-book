7ecd2862219397cbc07f75278ff1baccfa39629e
JoshSevy <joshuasevy@outlook.com>
2020-12-05 17:30:25 -0700

    Convert recipe components to work with new service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2019/06/Thai-Cucumber-Salad-Recipe-3.jpg'),
     new Recipe('Hamburger', 'A delicious perfectly cooked hamburger', 'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg')
