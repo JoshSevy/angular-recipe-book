3e71da3b4b39a40b89dfe7dc1dd52e8a5cf8972e
JoshSevy <joshuasevy@outlook.com>
2020-12-16 23:27:41 -0700

    Updating app to take in dynmic programmatic routes in recipe list and item

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,16 +10,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Tasty Schnitzel', 
-      'A super-tasty Shnitzel - just awesome!', 
+      'Tasty Schnitzel',
+      'A super-tasty Shnitzel - just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe(
-      'Big Fat Burger', 
-      'A delicious perfectly cooked hamburger', 
+      'Big Fat Burger',
+      'A delicious perfectly cooked hamburger',
       'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg',
       [
         new Ingredient('Meat', 1),
@@ -33,6 +33,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipeById(id: number) {
+    return this.recipes.find(recipe => recipe.id === id);
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]): void {
