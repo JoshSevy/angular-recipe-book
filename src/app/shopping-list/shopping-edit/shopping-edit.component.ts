0132f5a005341fa8db504f0fff435bd50d32ca3e
JoshSevy <joshuasevy@outlook.com>
2020-12-19 13:32:59 -0700

    Add remove indgredient functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -55,4 +55,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onRemove(index: number) {
+    this.slService.removeIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
 }
