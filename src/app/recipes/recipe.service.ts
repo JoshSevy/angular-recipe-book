45ea0b12581be503c20dec8c33cf0b31249382b3
JoshSevy <joshuasevy@outlook.com>
2020-12-22 17:14:54 -0700

    Fix bug that declares ingredients array if not present from fetch

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,51 +8,51 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Shnitzel - just awesome!',
-      'https://www.wininganddining.co.za/gallery/3022-24901-header.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(
-      'Big Fat Burger',
-      'A delicious perfectly cooked hamburger',
-      'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Buns', 2),
-        new Ingredient('tomato', 1),
-        new Ingredient('onion', 2)
-      ]),
-      new Recipe(
-      'Best Grilled Cheese',
-      'Amazing gouda and chedder grilled cheese',
-      'http://cleaneatsfastfeets.com/wp-content/uploads/2013/04/Italian-Grilled-Cheese.jpg',
-      [
-        new Ingredient('Sharp Chedder', 1),
-        new Ingredient('Sourdough Bread', 2),
-        new Ingredient('tomato', 1),
-        new Ingredient('pesto sauce', 1)
-      ]),
-      new Recipe(
-      'Vegan Chocolate Cream Pie',
-      'Vegan avacado and chocolate cream pie',
-      'http://hellyeahitsvegan.com/wp-content/uploads/2012/10/chocolate-cream-pie.jpg',
-      [
-        new Ingredient('cocoa nibs', 2),
-        new Ingredient('steel cut oats', 1),
-        new Ingredient('avacado', 1),
-        new Ingredient('cashew milk', 1),
-        new Ingredient('sugar', 2)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Shnitzel - just awesome!',
+  //     'https://www.wininganddining.co.za/gallery/3022-24901-header.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'A delicious perfectly cooked hamburger',
+  //     'https://makeyourmeals.com/wp-content/uploads/2019/03/air-fryer-hamburger.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('tomato', 1),
+  //       new Ingredient('onion', 2)
+  //     ]),
+  //     new Recipe(
+  //     'Best Grilled Cheese',
+  //     'Amazing gouda and chedder grilled cheese',
+  //     'http://cleaneatsfastfeets.com/wp-content/uploads/2013/04/Italian-Grilled-Cheese.jpg',
+  //     [
+  //       new Ingredient('Sharp Chedder', 1),
+  //       new Ingredient('Sourdough Bread', 2),
+  //       new Ingredient('tomato', 1),
+  //       new Ingredient('pesto sauce', 1)
+  //     ]),
+  //     new Recipe(
+  //     'Vegan Chocolate Cream Pie',
+  //     'Vegan avacado and chocolate cream pie',
+  //     'http://hellyeahitsvegan.com/wp-content/uploads/2012/10/chocolate-cream-pie.jpg',
+  //     [
+  //       new Ingredient('cocoa nibs', 2),
+  //       new Ingredient('steel cut oats', 1),
+  //       new Ingredient('avacado', 1),
+  //       new Ingredient('cashew milk', 1),
+  //       new Ingredient('sugar', 2)
+  //     ])
+  // ];
 
   constructor(private slService: ShoppingListService) {}
-
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
--- src/app/shared/Data-storage.service.ts
+++ src/app/shared/Data-storage.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
+
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from '../recipes/recipe.model';
@@ -23,6 +25,11 @@ export class DataStorageService {
      this.http.get<Recipe[]>(
        'https://ng-html-firebase-default-rtdb.firebaseio.com/recipes.json'
        )
+       .pipe(map(recipes => {
+         return recipes.map(recipe => {
+           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+         });
+       }))
        .subscribe(recipes => {
          this.recipeService.setRecipes(recipes)
        })
