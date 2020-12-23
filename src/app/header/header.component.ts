2c841a5ebd453c4051d5180651f9f069712d0435
JoshSevy <joshuasevy@outlook.com>
2020-12-22 17:33:37 -0700

    Add resolver for paths and data fetch bugs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,6 @@ export class HeaderComponent {
   }
 
   onFetchData() {
-    this.dataService.fetchRecipes();
+    this.dataService.fetchRecipes().subscribe();
   }
 }
--- src/app/shared/Data-storage.service.ts
+++ src/app/shared/Data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 
 import { RecipeService } from "../recipes/recipe.service";
@@ -22,17 +22,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-     this.http.get<Recipe[]>(
-       'https://ng-html-firebase-default-rtdb.firebaseio.com/recipes.json'
-       )
-       .pipe(map(recipes => {
-         return recipes.map(recipe => {
-           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-         });
-       }))
-       .subscribe(recipes => {
-         this.recipeService.setRecipes(recipes)
-       })
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-html-firebase-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
+        })
+      )
   }
 
 }
