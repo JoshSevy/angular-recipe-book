facc53ba5387f481deb69507115e87fd95cdcc04
JoshSevy <joshuasevy@outlook.com>
2020-12-23 13:41:51 -0700

    Add loading spinner to signup signin form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 
 export class AuthComponent {
   isLoginMode = true;
-  authEndpoint: string = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[API_KEY]"
+  isLoading = false;
 
   constructor(private authService: AuthService) {}
 
@@ -23,14 +23,17 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       //...
     } else {
       this.authService.signup(email, password)
         .subscribe(resData => {
           console.log(resData);
+          this.isLoading = false;
         }, error => {
           console.log(error.message);
+          this.isLoading = false;
         })
     }
 
