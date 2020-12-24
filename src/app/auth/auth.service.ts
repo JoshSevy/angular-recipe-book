0687671b339bac195f7569e3115270699c737e3b
JoshSevy <joshuasevy@outlook.com>
2020-12-23 22:45:56 -0700

    Add local storage for user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,7 +22,6 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
-
   constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
@@ -67,6 +66,23 @@ export class AuthService {
     );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) return;
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -89,6 +105,7 @@ export class AuthService {
           );
 
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
       }
 
 
