1e4dcadbdb8f5402d88f10c875e78aa2ba3e4048
JoshSevy <joshuasevy@outlook.com>
2021-01-02 18:09:55 -0700

    Move child recipe routes to own module and connect it to recipe module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 import { RecipesComponent } from "./recipes.component";
 
 @NgModule({
@@ -28,11 +29,12 @@ import { RecipesComponent } from "./recipes.component";
     RecipeEditComponent,
   ],
   imports: [
-    RouterModule,
+    RecipesRoutingModule,
     CommonModule,
     ReactiveFormsModule
   ]
 })
+
 export class RecipesModule {
 
 }
