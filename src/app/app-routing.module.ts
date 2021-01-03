5071282ea5be730d0ebd4dd8927909b52beaf04f
JoshSevy <joshuasevy@outlook.com>
2021-01-03 13:43:50 -0700

    Add lazy loading for shopping list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes',
-    loadChildren: './recipes/recipes.module.ts#RecipesModule'
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
   }
 ];
 
