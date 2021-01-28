19af4f2a9ccdbb1f72421846eeab854df2c81186
JoshSevy <joshuasevy@outlook.com>
2021-01-28 11:21:25 -0700

    Connect store to update and remove ingredients in shopping list component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -44,7 +44,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      // this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient}))
     } else {
       // this.slService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
@@ -59,7 +60,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onRemove(index: number) {
-    this.slService.removeIngredient(this.editedItemIndex);
+    // this.slService.removeIngredient(this.editedItemIndex);
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex))
     this.onClear();
   }
 
