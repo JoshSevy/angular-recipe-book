aa2c70627d3168b42271c8909060a7ba44153325
JoshSevy <joshuasevy@outlook.com>
2021-01-28 14:27:15 -0700

    Add auth actions to replace auth service

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 import { User } from "../user.model";
 
 export interface State {
@@ -8,6 +10,15 @@ const initialState: State = {
   user: null
 }
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(
+  state = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+
+    case AuthActions.LOGOUT:
+    default:
+      return state;
+  }
 }
