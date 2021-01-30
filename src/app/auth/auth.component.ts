519e8d7da01760a7c8d759aacbbb3f22297b90a5
JoshSevy <joshuasevy@outlook.com>
2021-01-30 14:43:37 -0700

    Refactor auth component to accept store actions

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -55,26 +55,10 @@ export class AuthComponent implements OnDestroy, OnInit {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      // authObs = this.authService.login(email, password)
       this.store.dispatch(new AuthActions.LoginStart({ email: email, password: password }))
     } else {
-      authObs = this.authService.signup(email, password);
+      this.store.dispatch(new AuthActions.SignupStart({ email: email, password: password }))
     }
-
-
-    // authObs.subscribe(
-    //   resData => {
-    //     console.log(resData);
-    //     this.isLoading = false;
-    //     this.router.navigate(['./recipes']);
-    //   },
-    //   errorMessage => {
-    //     console.log(errorMessage);
-    //     this.error = errorMessage;
-    //     this.showErrorAlert(errorMessage);
-    //     this.isLoading = false;
-    //   }
-    // )
     form.reset();
   }
 
