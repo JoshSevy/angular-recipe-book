9c4ab16eeb6b2742b6119b5ebcb4bdd1d0d67cd1
JoshSevy <joshuasevy@outlook.com>
2021-01-29 19:08:09 -0700

    Add login request to effects

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,13 +1,52 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, switchMap, map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 import * as AuthActions from './auth.actions';
 
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
 export class AuthEffects {
+  @Effect()
   authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPI}`,
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true
+        }
+      ).pipe(catchError(error => {
+        //...
+        return of();
+      }), map(resData => {
+        const expirationDate = new Date(
+          new Date().getTime() + +resData.expiresIn * 1000
+          );
+        return of(new AuthActions.Login({
+          email: resData.email,
+          userId: resData.localId,
+          token: resData.idToken,
+          expirationDate: expirationDate
+        }));
+      }))
+    }),
+
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private http: HttpClient) {}
 
 
 }
