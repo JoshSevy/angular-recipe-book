71219a1221627e8952097fb85a112037242c27e7
JoshSevy <joshuasevy@outlook.com>
2021-01-30 19:22:33 -0700

    Add redux dev tools and router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,11 +13,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 
 import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
-
-
-
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,6 +29,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     EffectsModule.forRoot([AuthEffects]),
     SharedModule,
     CoreModule,
+    StoreDevtoolsModule.instrument({ logOnly: environment.production })
 
   ],
   bootstrap: [AppComponent]
