85d243b62da1433d8ec07cbc399528b4c8806ad4
JoshSevy <joshuasevy@outlook.com>
2021-01-30 17:33:50 -0700

    Add auto login and auto logout effects to store

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -27,7 +27,7 @@ const handleAuthentication = (
 ) => {
   const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
   const user = new User(email, userId, token, expirationDate);
-  // localStorage.setItem('userData', JSON.Stringify(user)');
+  localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
     email: email,
     userId: userId,
@@ -123,6 +123,50 @@ export class AuthEffects {
     this.router.navigate(['/']);
     })
   );
+
+  @Effect()
+  autoLogin = this.actions$.pipe(
+    ofType(AuthActions.AUTO_LOGIN),
+    map(() => {
+      const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) return { type: 'DUMMY' };
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      // this.user.next(loadedUser);
+      return new AuthActions.AuthenticateSuccess({
+          email: loadedUser.email,
+          userId: loadedUser.id,
+          token: loadedUser.token,
+          expirationDate: new Date(userData._tokenExpirationDate)
+        })
+    //   const experationDuration =
+    //     new Date(userData._tokenExpirationDate).getTime() -
+    //     new Date().getTime();
+    //   this.autoLogout(experationDuration);
+    // }
+      }
+      return { type: 'DUMMY' }
+    })
+  );
+
+
+  @Effect()
+  autoLogout = this.actions$.pipe(ofType(AuthActions.LOGOUT), tap(() => {
+    localStorage.removeItem('userData');
+  }))
 
   constructor(
     private actions$: Actions,
