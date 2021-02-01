de25edbbd23d8aa893d405a2c052886d8a10bb8e
JoshSevy <joshuasevy@outlook.com>
2021-02-01 14:41:04 -0700

    Create fetch effect to move request data to ngrx

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,8 +1,32 @@
-import { Actions, Effect } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, map } from 'rxjs/operators';
+
+import * as RecipesActions from '../store/recipe.actions';
+import { Recipe } from '../recipe.model';
 
 export class RecipeEffects {
   @Effect()
-  fetchRecipes
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipesActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http.get<Recipe[]>(
+      'https://ng-html-firebase-default-rtdb.firebaseio.com/recipes.json'
+      )
+    }),
+    map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+    map(recipes => {
+      return new RecipesActions.SetRecipes(recipes);
+    })
+  );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private http: HttpClient) {}
+
 }
