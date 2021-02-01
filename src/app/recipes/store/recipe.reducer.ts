a0eea0268333902b6c2daa94b2c81f5ad436049a
JoshSevy <joshuasevy@outlook.com>
2021-02-01 15:22:01 -0700

    Update recipe reducers for new actions

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,5 +1,6 @@
+import { act } from '@ngrx/effects';
 import { Recipe } from '../recipe.model';
-import * as RecipesActions from './recipe.actions';
+import * as RecipeActions from './recipe.actions';
 
 export interface State {
   recipes: Recipe[];
@@ -9,13 +10,43 @@ const initialState: State = {
   recipes: []
 }
 
-export function recipeReducer(state = initialState, action: RecipesActions.RecipesActions) {
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
   switch (action.type) {
-    case RecipesActions.SET_RECIPES:
+    case RecipeActions.SET_RECIPES:
       return {
         ...state,
         recipes: [...action.payload]
       };
+
+    case RecipeActions.ADD_RECIPE:
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+
+    case RecipeActions.UPDATE_RECIPE:
+      const updatedRecipe = {
+        ...state.recipes[action.payload.index],
+        ...action.payload.newRecipe
+      }
+
+      const updatedRecipes = [...state.recipes];
+      updatedRecipes[action.payload.index] = updatedRecipe;
+
+      return {
+        ...state,
+        recipes: updatedRecipes
+      };
+
+    case RecipeActions.DELETE_RECIPE:
+      return {
+        ...state,
+        recipes: state.recipes.filter((recipe, index) => {
+          return index !== action.payload;
+        })
+      };
+
+
     default:
       return state;
   }
