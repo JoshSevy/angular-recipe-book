b029361312bd4d991cea320d45ac3a7e8bb930bf
JoshSevy <joshuasevy@outlook.com>
2021-02-02 16:05:47 -0700

    Connect angular universal to server load parts of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import * as fromAuth from './auth/store/auth.reducer';
     HeaderComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({auth: fromAuth.authReducer}),
